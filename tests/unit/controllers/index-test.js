12ead662d02419c0d709a11aa5ecb8681d7a175a
Asa Smith <smith.asa.la@gmail.com>
2019-08-06 16:06:35 -0400

    another test :white_check_mark:

--- tests/unit/controllers/index-test.js
+++ tests/unit/controllers/index-test.js
@@ -4,9 +4,15 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | index', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:index');
-    assert.ok(controller);
-  });
+  // testing the saveInvitation action
+  test('submit email function', function (assert) {
+    const controller = this.owner.lookup('controller:index')
+    const email = `test@test.test`
+
+    controller.set('emailAddress', email)
+    controller.send('saveInvitation')
+
+    assert.equal(controller.get('emailAddress'), '')
+    assert.equal(controller.get('responseMessage'), `thank you we just saved your email ${email}`)
+  })
 });
